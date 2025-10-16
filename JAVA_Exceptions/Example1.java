0b54b83f1758f7146d058d0c67364c782a07c604
chrisszlycoen <uhiriwechrisostom0@gmail.com>
2025-10-16 09:23:16 +0200

    Restored deleted files

--- JAVA_Exceptions/Example1.java
+++ JAVA_Exceptions/Example1.java
@@ -2,9 +2,12 @@ package JAVA_Exceptions;
 public class Example1 {
     public static void main(String[] args) {
         int a,b,c;
-        a = 2;
+        System.out.println("exception");
+        a = 0;
         b = 3;
         c = (b/a);
+        System.out.println("exception 2");
         System.out.println(c);
+
     }
 }
